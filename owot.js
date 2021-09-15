533a2fd2f4a22cd8844dae657837e99f1b75f919
scar17off <81940406+scar17off@users.noreply.github.com>
2021-09-15 19:40:42 +0300

    Update owot.js

--- owot.js
+++ owot.js
@@ -1,6 +1,51 @@
-var OWOTJS = class OWOTJS{
-	constructor(){
+var WebSocket = require('ws');
 
+var OWOTJS = class OWOTJS{
+	constructor(options = {}){
+		super();
+		var OTS = this;
+
+		OTS.player {
+			color: null,
+			rank: null,
+			id: null
+			x: 0,
+			y: 0
+		};
+		OTS.options = {
+			origin: options,
+			ws: ws,
+			chunkSize: 16
+		};
+		OTS.chat = {
+			setNickname: function(nickname) {
+				ws.send(`/nick `+nickname);
+			};
+			send: function(tosend) {
+				ws.send(msg);
+			};
+		};
+		OTS.world = {
+			setChar: function(char,x,y) {
+				
+			};
+			connect: function() {
+				var ws = new WebSocket(, undefined, {
+                	headers: {'Origin': OTS.options.origin}
+            	});
+			};
+			join: function (world) {
+
+			};
+			leave: function(world) {
+
+			};
+		};
+		OTS.misc = {
+			teleport: function(x,y) {
+
+			};
+
+		};
 	};
 };
-var owot = new OWOTJS();
