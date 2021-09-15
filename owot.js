ff3a4131f42755a86162eb1316f14ca83b211856
scar17off <81940406+scar17off@users.noreply.github.com>
2021-09-15 20:04:44 +0300

    Update owot.js

--- owot.js
+++ owot.js
@@ -5,7 +5,69 @@ var OWOTJS = class OWOTJS{
 		super();
 		var OTS = this;
 
+		OTS.player {var WebSocket = require('ws');
+
+class OWOTjs{
+	constructor(options = {}){
+		super();
+		var OTS = this;
+
 		OTS.player {
+			color: null,
+			rank: null,
+			id: null
+			x: 0,
+			y: 0
+		};
+		OTS.options = {
+			origin: options.origin,
+			ws: options.ws,
+			chunkSize: 16,
+			playerJoined: false;
+		};
+		OTS.chat = {
+			setNickname: function(nickname) {
+				if(!ws) return;
+				else continue;
+				ws.send(`/nick `+nickname);
+			};
+			send: function(tosend) {
+				if(!ws) return;
+				else continue;
+				ws.send(msg);
+			};
+		};
+		OTS.world = {
+			setChar: function(char, x, y) {
+				ws.send(array);
+			};
+			connect: function() {
+				var ws = new WebSocket(, undefined, {
+                	headers: {'Origin': OTS.options.origin}
+            	});
+            	ws.on("open", () => {
+            		if(!ws) return;
+					else continue;
+					OTS.player.joined = true;
+            	});
+			};
+			leave: function() {
+				ws.close();
+				OTS.player.joined = false;
+			};
+			setColor: function() {
+				ws.send(array);
+			}
+		};
+		OTS.misc = {
+			teleport: function(x,y) {
+
+			};
+		};
+	};
+};
+
+module.exports = {OTS};
 			color: null,
 			rank: null,
 			id: null
